2d30486e55ea6f1b9bd033fab0021de5a1e77a4a
boche2506-cmd <boche2506@gmail.com>
2026-06-05 20:51:22 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ function updateUIForUser(user, currentRole) {
         } else if (currentRole === "seller") {
             userNameDisplay.innerHTML = `🏪 老闆`;
         } else {
-            userNameDisplay.innerHTML = `<img src="logo.png" alt="買家圖示" style="width: 1.5rem; height: 1.5rem;"> 買家`;
+            userNameDisplay.innerHTML = `<img src="logo.png" alt="買家圖示" style="width: 2rem; height: 2rem;"> 買家`;
         }
     }
 

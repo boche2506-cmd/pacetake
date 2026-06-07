ced442efa1d48361b1aed0f530e6e98cc6d452c9
boche2506-cmd <boche2506@gmail.com>
2026-06-08 02:06:31 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -171,8 +171,7 @@ function updateUIForUser(user, currentRole) {
         } else if (currentRole === "seller") {
             userNameDisplay.innerHTML = `🏪 老闆`;
         } else {
-            userNameDisplay.innerHTML = `<img src="logo.png" class="buyer" alt="買家圖示" style="
-            height: 100%; max-width: 30%; font-weight: 500; padding: 2cqw 4cqw; border-radius: 10cqw; font-size: 4cqw; cursor: pointer; display: flex; flex-direction: row; justify-content: center; align-items: center; "> 買家`;
+            userNameDisplay.innerHTML = `<img src="logo.png" class="buyer" alt="買家圖示" style="> 買家`;
         }
     }
 
